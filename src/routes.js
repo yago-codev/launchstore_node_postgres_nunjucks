9faf4487c938136e8c3b2a41e581b5ccccfb7597
yago-codev <yagomilano92@gmail.com>
2020-03-23 00:29:35 -0300

    criando rota e view para cadastrar produtos

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const routes = express.Router();
 
+const ProductController = require('./app/controllers/ProductController');
+
 routes.get('/', (req, res) => {
   return res.render('layout.njk');
+});
+
+routes.get('/products/create', ProductController.create);
+
+routes.get('/ads/create', (req, res) => {
+  return res.redirect('/products/create');
 });
 
 module.exports = routes;
